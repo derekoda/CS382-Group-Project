a68fbc2af147e834f84304d9dfc01482d9540e89
kurt-caves <kurtalyell@gmail.com>
2023-04-16 12:13:08 -0600

    added db.py, schema.sql. Transferred over schema from mongoose to sqlite, modified app.py to use the db and added some content to contribute.html to add and query areas in building schema.

---
+++ db.py
@@ -0,0 +1,45 @@
+import sqlite3
+import click
+from flask import g, current_app
+from flask.cli import with_appcontext
+
+DATABASE = 'afpwnsqt.db'
+
+# This function is used to get the database.
+# It is used in the app.py file to get the database.
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            DATABASE,
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+# This function is used to close the database.
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+# This function is used to initialize the database.
+def init_db():
+    with current_app.app_context():
+        db = get_db()
+        with current_app.open_resource('schema.sql', mode='r') as f:
+            db.executescript(f.read())
+
+# This function is used to initialize the database.
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+# used to initialize the app with the database.
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
---
+++ schema.sql
@@ -0,0 +1,48 @@
+CREATE TABLE area (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT NOT NULL CHECK (type IN ('computer', 'single_study', 'group_study', 'outlet', 'whiteboard', 'printer', 'quiet', 'loud', 'large_desk', 'small_desk', 'medium_desk', 'padded_chair', 'non_padded_chair', 'window', 'no_window', 'near_exit')),
+    count INTEGER NOT NULL,
+    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE area_comment (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    area_id INTEGER NOT NULL,
+    body TEXT,
+    date TIMESTAMP,
+    FOREIGN KEY (area_id) REFERENCES area (id) ON DELETE CASCADE
+);
+
+CREATE TABLE area_meta (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    area_id INTEGER NOT NULL,
+    votes INTEGER,
+    favs INTEGER,
+    FOREIGN KEY (area_id) REFERENCES area (id) ON DELETE CASCADE
+);
+
+CREATE TABLE machine (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT NOT NULL CHECK (type IN ('soda', 'snack', 'energy_drink')),
+    count INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE floor (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    floorNumber INTEGER NOT NULL,
+    bathrooms INTEGER DEFAULT 0
+);
+
+CREATE TABLE floor_area (
+    floor_id INTEGER NOT NULL,
+    area_id INTEGER NOT NULL,
+    FOREIGN KEY (floor_id) REFERENCES floor (id) ON DELETE CASCADE,
+    FOREIGN KEY (area_id) REFERENCES area (id) ON DELETE CASCADE
+);
+
+CREATE TABLE floor_machine (
+    floor_id INTEGER NOT NULL,
+    machine_id INTEGER NOT NULL,
+    FOREIGN KEY (floor_id) REFERENCES floor (id) ON DELETE CASCADE,
+    FOREIGN KEY (machine_id) REFERENCES machine (id) ON DELETE CASCADE
+);
